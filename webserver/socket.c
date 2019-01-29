b25943e852686a033da617e057faad2be0133e03
BauvinRaphael <raphael.bauvin.etu@univ-lille.fr>
2019-01-29 11:17:02 +0100

    correction

--- webserver/socket.c
+++ webserver/socket.c
@@ -1,2 +1,13 @@
+int creer_serveur ( int port ){
+    
+    int socket_serveur ;
+    socket_serveur = socket ( AF_INET , SOCK_STREAM , 0);
+    if ( socket_serveur == -1)
+    {
+        /* traitement de l ’ erreur */
+        perror ( " socket_serveur " );
+    }
+    /* Utilisation de la socket serveur */
 
-int socket(int domain, int type, int protocol);
+    return 0;
+}
